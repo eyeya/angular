289d4c1c778d3d6de415204e80164c56fc57e9ef
eyeya <86484894+eyeya@users.noreply.github.com>
2021-11-29 22:50:51 +0700

    week 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './size/navbar/navbar.component';
+import { SlideComponent } from './size/slide/slide.component';
+import { RegisterComponent } from './size/register/register.component';
+import { FooterComponent } from './size/footer/footer.component';
+import { LoginComponent } from './size/login/login.component';
+import { ProflieComponent } from './size/proflie/proflie.component';
+import { AboutComponent } from './size/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SlideComponent,
+    RegisterComponent,
+    FooterComponent,
+    LoginComponent,
+    ProflieComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
