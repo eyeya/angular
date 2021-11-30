af4052e87957e864cacc78e7671023d1b5f1deef
eyeya <86484894+eyeya@users.noreply.github.com>
2021-12-01 01:22:44 +0700

    week3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { ProductComponent } from './admin/product/product.component';
+import { StatusComponent } from './admin/status/status.component';
+import { UsersComponent } from './admin/users/users.component';
+import { SiteComponent } from './site/site.component';
+import { AboutComponent } from './size/about/about.component';
+import { ProflieComponent } from './size/proflie/proflie.component';
+import { SlideComponent } from './size/slide/slide.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SiteComponent,
+    children: [
+      { path: '', component: SlideComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'profile', component: ProflieComponent },
+      { path: 'slide', component: SlideComponent },
+    ]
+  },
+  {
+    path:'',component:AdminComponent,
+    children:[
+      {path:'admin',component:AdminComponent},
+      {path:'users',component:UsersComponent},
+      {path:'status',component:StatusComponent},
+      {path:'product',component:ProductComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,15 @@ import { FooterComponent } from './size/footer/footer.component';
 import { LoginComponent } from './size/login/login.component';
 import { ProflieComponent } from './size/proflie/proflie.component';
 import { AboutComponent } from './size/about/about.component';
+import { SiteComponent } from './site/site.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { StatusComponent } from './admin/status/status.component';
+import { UsersComponent } from './admin/users/users.component';
+import { ProductComponent } from './admin/product/product.component';
+import { DashboardNavbarComponent } from './dashboard-navbar/dashboard-navbar.component';
+import { DashboardSidebarComponent } from './dashboard-sidebar/dashboard-sidebar.component';
+import { DashboardFooterComponent } from './dashboard-footer/dashboard-footer.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +29,16 @@ import { AboutComponent } from './size/about/about.component';
     FooterComponent,
     LoginComponent,
     ProflieComponent,
-    AboutComponent
+    AboutComponent,
+    SiteComponent,
+    AdminComponent,
+    DashboardComponent,
+    StatusComponent,
+    UsersComponent,
+    ProductComponent,
+    DashboardNavbarComponent,
+    DashboardSidebarComponent,
+    DashboardFooterComponent
   ],
   imports: [
     BrowserModule,
